c95ffe6a85daa7f720270c4a9e0ed095ed4b7b19
Katarzyna Smoliniec <katarzyna.smoliniec.programista@gmail.com>
2023-06-07 15:18:04 +0200

    Add file to project

---
+++ homeBudget/auxiliaryMethods.cpp
@@ -0,0 +1,83 @@
+#include "MetodyPomocnicze.h"
+
+string MetodyPomocnicze :: konwerjsaIntNaString(int liczba)
+{
+    ostringstream ss;
+    ss << liczba;
+    string str = ss.str();
+    return str;
+}
+
+int MetodyPomocnicze :: konwersjaStringNaInt(string liczba)
+{
+    int liczbaInt;
+    istringstream iss(liczba);
+    iss >> liczbaInt;
+
+    return liczbaInt;
+}
+
+string MetodyPomocnicze :: wczytajLinie()
+{
+    string wejscie = "";
+    cin.sync();
+    getline(cin, wejscie);
+    return wejscie;
+}
+
+string MetodyPomocnicze :: zamienPierwszaLitereNaDuzaAPozostaleNaMale(string tekst)
+{
+    if (!tekst.empty())
+    {
+        transform(tekst.begin(), tekst.end(), tekst.begin(), ::tolower);
+        tekst[0] = toupper(tekst[0]);
+    }
+    return tekst;
+}
+
+string MetodyPomocnicze :: pobierzLiczbe(string tekst, int pozycjaZnaku)
+{
+    string liczba = "";
+    while(isdigit(tekst[pozycjaZnaku]) == true)
+    {
+        liczba += tekst[pozycjaZnaku];
+        pozycjaZnaku ++;
+    }
+    return liczba;
+}
+
+char MetodyPomocnicze :: wczytajZnak()
+{
+    string wejscie = "";
+    char znak  = {0};
+
+    while(true)
+    {
+        string wejscie = wczytajLinie();
+
+        if (wejscie.length() == 1)
+        {
+            znak = wejscie[0];
+            break;
+        }
+        cout << "To nie jest pojedynczy znak. Wpisz ponownie." << endl;
+    }
+    return znak;
+}
+
+int MetodyPomocnicze :: wczytajLiczbeCalkowita()
+{
+    string wejscie = "";
+    int liczba = 0;
+
+    while(true)
+    {
+        getline(cin, wejscie);
+
+        stringstream myStream(wejscie);
+        if (myStream >> liczba)
+            break;
+        cout << "To nie jest liczba. Wpisz ponownie. " << endl;
+    }
+    return liczba;
+}
